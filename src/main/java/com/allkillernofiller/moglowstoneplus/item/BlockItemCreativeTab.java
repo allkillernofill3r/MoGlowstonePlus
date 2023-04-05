1677ca03f7878adecda59102e56c55543716d9d4
MasterGeneral156 <MasterGeneral156@users.noreply.github.com>
2023-04-04 22:31:15 -0400

    Remove itemgroup from Item class, into event

--- src/main/java/com/allkillernofiller/moglowstoneplus/item/BlockItemCreativeTab.java
+++ src/main/java/com/allkillernofiller/moglowstoneplus/item/BlockItemCreativeTab.java
@@ -1,13 +1,11 @@
 package com.allkillernofiller.moglowstoneplus.item;
 
-import com.themastergeneral.moglowstone.MoGlowstone;
-
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 
 public class BlockItemCreativeTab extends BlockItem {
     public BlockItemCreativeTab(Block block) {
-        super(block, new Item.Properties().tab(MoGlowstone.ITEMGROUP));
+        super(block, new Item.Properties());
     }
 }
